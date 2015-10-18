16b36288925952e6ab67c7bc2eaf7ad4ba97cccc
PatrickPullum <PApullum@csustudent.net>
2015-10-18 13:19:07 -0400

    Class Diagram. Committed and pushed 10/18/15 @ 1:18PM

---
+++ Connect4 Planning/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//The diagram is saved as Connect4ClassDia*/
+        //If it cannot be found please notify Patrick right away */
+    }
+}
